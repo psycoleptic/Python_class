a17df7c0700da21bad61e2e23214faf1db667bed
psycoleptic <32726612+psycoleptic@users.noreply.github.com>
2018-11-15 10:25:42 +0700

    Add files via upload

---
+++ Functions Task 1.py
@@ -0,0 +1,22 @@
+#1
+# def hello(name="Polina"):
+#    print("Hello", input())
+#    return name
+# print(hello())
+#2
+# def none(n = "print"):
+#   print(input())
+#    return n
+# print(none())
+
+#4
+#def fac(n):
+#   r = 1
+#    for i in range(2, n + 1):
+ #       r *= i
+#    return r
+#print(fac(18))
+
+#5
+N = int(input)
+
---
+++ Functions.py
@@ -0,0 +1,24 @@
+#def my_pow(n, f_name = "my_pow", power=2) -> int:
+  #  print("work", f_name)
+ #   return n**power
+#print(my_pow(10))
+#print(my_pow(10, 3))
+#print(my_pow(10, power = 4))
+
+#def my_rec(n):
+#    print(n)
+ #   if n<3:
+#        return
+#    return(my_rec(n-1))
+
+#my_rec(10)
+
+#def hello(name = "Polina"):
+#    print('Hello', input())
+#hello()
+#print(hello("Alex"))
+
+#def hello():
+#    return 1, 2
+#x, y = hello()
+#print(y,x)
